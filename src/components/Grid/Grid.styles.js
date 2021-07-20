7f057835002a40875fbb01b0590befb9d6b01fdd
TomcyT <tomcy48@gmail.com>
2021-07-21 03:39:17 +0530

    Added responsiveness to the grid

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -4,12 +4,23 @@ export const Wrapper = styled.div`
   max-width: var(--maxWidth);
   margin: 0 auto;
   padding: 0 20px;
+  animation: animateText 0.5s;
+
+  @keyframes animateText {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 
   h1 {
     color: var(--medGrey);
+    font-size: var(--fontBig);
 
-    @media screen and (maxwidth: 720px) {
-      font-size: var(--fontBig);
+    @media screen and (max-width: 720px) {
+      font-size: var(--fontMed);
     }
   }
 `;
